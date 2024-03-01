9ea95b7716f8d685a9c0453a03a73d7e496aa5aa
Danil <danil.belanokov@gmail.com>
2024-03-01 14:34:16 +0300

    Here goes a fork

--- program.py
+++ program.py
@@ -1,9 +1,15 @@
+def new_function():
+    pass
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
     print('And again')
     print('And to check origin tag')
     print('And also check how to edit code on GitHub platform')
+
+    new_function()
 
 
 if __name__ == '__main__':

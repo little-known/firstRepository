fddb1b98cee60ae55d2223546522d423858bbf41
little-known <161714873+little-known@users.noreply.github.com>
2024-03-01 14:11:12 +0300

    I commited this one online

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ def main():
     print('And I change it every day')
     print('And again')
     print('And to check origin tag')
+    print('And also check how to edit code on GitHub platform')
 
 
 if __name__ == '__main__':
